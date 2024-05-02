750effc25a5951bb49f0e031e89a042d21d2a5be
monepdepresot <120069627+monepdepresot@users.noreply.github.com>
2024-05-03 01:03:56 +0700

    2

---
+++ Landmarks[1]/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @State private var modelData = ModelData()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(modelData)
+        }
+
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+    }
+}
